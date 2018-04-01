ac359bf70193a8fb8a2adb8ec4db6a1cfbc1e6bf
Kassiane <kassiane@gmail.com>
2018-04-02 01:59:00 +0200

    Chaging data to take from video collection

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -8,7 +8,7 @@ import './body.html';
 import './chart_example.js';
 
 const Videos = new Mongo.Collection('videos');
-const ChartData = new Mongo.Collection('chart');
+// const ChartData = new Mongo.Collection('chart');
 
 Template.body.onCreated(function bodyOnCreated() {
 	Meteor.subscribe('videos');
@@ -41,3 +41,38 @@ Template.body.events({
 		target.text.value = '';
 	},
 });
+
+var chart;
+
+Template.acTemplate.rendered = function() {
+  /*
+    Get container for chart.
+    It is not actually necessary here, `chart.container('container').draw();` can be used
+    for current scope, but container is found in template to avoid container ID duplication.
+   */
+  var container = this.find("#container");
+
+  // Turn Meteor Collection to simple array of objects.
+  var videoData = Videos.find({}).fetch();
+
+  var data = [];
+
+  videoData.forEach(function(element) {
+  	data.push({x: element.title, value: element.id});
+  });
+  
+  console.log('Aqui');
+  console.log(data);
+  //  ----- Standard Anychart API in use -----
+  chart = anychart.pie(data);
+  chart.title('ACME Corp. apparel sales through different retail channels');
+
+  chart.legend()
+      .position('bottom')
+      .itemsLayout('horizontal')
+      .align('center')
+      .title('Retail channels');
+
+  chart.animation(true);
+  chart.container(container).draw();
+};
